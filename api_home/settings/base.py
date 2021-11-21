4e56d5e7e4524126ee79e263bb1c26f8b2a2712e
thiagovl <contatothiagolemes@gmail.com>
2021-11-20 21:46:00 -0300

    Alterando o settings.py

--- api_home/settings/base.py
+++ api_home/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*', config('ALLOWED_HOSTS', default=[], cast=str)]
 
