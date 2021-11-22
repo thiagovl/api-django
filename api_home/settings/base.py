d1de77e319d3c81586d941367eec3c2068e10fff
thiagovl <contatothiagolemes@gmail.com>
2021-11-22 07:37:14 -0300

    Alterado as configuraçoes

--- api_home/settings/base.py
+++ api_home/settings/base.py
@@ -151,6 +151,7 @@ DATABASES = {
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
